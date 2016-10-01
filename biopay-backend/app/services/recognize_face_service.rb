54a65d928ed97374ad6be6d752c5ca55bc99f4c6
Fabian Kübler <fabian.kuebler@zweitag.de>
2016-10-01 18:11:14 +0200

    update face recog

--- biopay-backend/app/services/recognize_face_service.rb
+++ biopay-backend/app/services/recognize_face_service.rb
@@ -11,18 +11,26 @@ class RecognizeFaceService
     begin
       result = RestClient.post "#{api_base_url}/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceId=true&returnFaceLandmarks=false", File.new(path, 'rb'), content_type: 'application/octet-stream; charset=binary', :ocp_apim_subscription_key => api_key
 
-      face_id = JSON.parse(result.body).first["faceId"]
+      parsed_results = JSON.parse(result.body)
+      return {result: "fail", message: "No face detected."} if parsed_results.empty?
+
+      face_id = parsed_results.first["faceId"]
+
       identification_result = RestClient.post "#{api_base_url}/v1.0/identify", {personGroupId: person_group, faceIds: [face_id], maxNumOfCandidatesReturned: 1, confidenceThreshold: "0.5"}.to_json, content_type: 'json', :ocp_apim_subscription_key => api_key
       candidate = JSON.parse(identification_result.body).first["candidates"].first
 
+      return {result: "fail", message: "No known face recognized."} if JSON.parse(identification_result.body).first["candidates"].empty?
+
       person_details_result = RestClient.get "#{api_base_url}/v1.0/persongroups/#{person_group}/persons/#{candidate["personId"]}", content_type: 'json', :ocp_apim_subscription_key => api_key
-      person_details = person_details_result.body
+      person_details = JSON.parse(person_details_result.body)
       puts "identified #{person_details} with confidence #{candidate["confidence"]}"
-      person_details
+
+      user_data = JSON.parse(person_details["userData"])
+      return {result: "success", name: person_details["name"], iban: user_data["iban"]}
     rescue RestClient::ExceptionWithResponse => e
-      puts e.response.body
+      return {result: "fail", message: JSON.parse(e.response.body)["error"]["message"]}
     end
   end
 end
 
-#RecognizeFaceService.('./lib/tasks/Fabian/1.jpg')
+#RecognizeFaceService.('./public/demo.jpg')
